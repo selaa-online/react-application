c03cdeef4a458d6e76e2d32eee8fda20be87df34
Selaka Nanayakkara <selaka.kithmal@gmail.com>
2021-05-07 01:49:13 +0530

    added realistic data fetching from youtube api

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search-bar';
 const API_KEY = 'AIzaSyDw-UIFDz1-u4kmryuIqck_IAAF84ksV3c';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video-list';
 
 
-const App = () => {
-  return (<div>
-    <SearchBar />
-  </div>);
+class App extends Component {
+
+
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, (allVideos) => {
+      this.setState({ videos: allVideos });
+    });
+  }
+  render() {
+    return (<div>
+      <SearchBar />
+      <VideoList videos={this.state.videos} />
+    </div>);
+  }
 }
 
 ReactDOM.render(
