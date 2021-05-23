69f0417b6d980aafa89fb636f927975230f658aa
Selaka Nanayakkara <selaka.kithmal@gmail.com>
2021-05-23 20:45:19 +0530

    [9] Video selection from the list

--- src/index.js
+++ src/index.js
@@ -4,23 +4,30 @@ import SearchBar from './components/search-bar';
 const API_KEY = 'AIzaSyDw-UIFDz1-u4kmryuIqck_IAAF84ksV3c';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video-list';
-
+import VideoDetail from './components/video-detail';
 
 class App extends Component {
 
 
   constructor(props) {
     super(props);
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     YTSearch({ key: API_KEY, term: 'surfboards' }, (allVideos) => {
-      this.setState({ videos: allVideos });
+      this.setState({
+        videos: allVideos,
+        selectedVideo: allVideos[0]
+      });
     });
   }
   render() {
     return (<div>
       <SearchBar />
-      <VideoList videos={this.state.videos} />
+      <VideoDetail video={this.state.selectedVideo} />
+      <VideoList onVideoSelect={selectedVideo => this.setState({ selectedVideo })} videos={this.state.videos} />
     </div>);
   }
 }
